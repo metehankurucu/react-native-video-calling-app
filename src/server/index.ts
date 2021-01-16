08494104175e95416f2b0262411bdb27e9002edf
Metehan Kurucu <43188732+metehankurucu@users.noreply.github.com>
2021-01-16 15:59:17 +0300

    delete server urls

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,4 @@
-// export const PEER_SERVER_HOST = '192.168.1.101';
-// export const PEER_SERVER_PORT = 9000;
-// export const PEER_SERVER_PATH = '/peerjs';
-// export const SERVER_URL = 'http://192.168.1.101:3000';
-export const PEER_SERVER_HOST = 'webrtc-peer-server.herokuapp.com';
+export const PEER_SERVER_HOST = '';
 export const PEER_SERVER_PORT = '';
-export const PEER_SERVER_PATH = '/peerjs';
-export const SERVER_URL = 'https://webrtc-socket-api.herokuapp.com/';
+export const PEER_SERVER_PATH = '';
+export const SERVER_URL = '';
